bef055b0b10f58c4b39c8b7d75af3fb772455770
Györfi Marcell <gyorfimarcell@gmail.com>
2024-10-02 11:52:45 +0200

    vége

--- desserts-frontend/src/components/Cart.tsx
+++ desserts-frontend/src/components/Cart.tsx
@@ -1,10 +1,15 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartItem from "./CartItem";
 import { CartContext } from "../pages/Home";
 
 export type CartItemType = {
     name: string;
     price: number;
+};
+
+export type VisibleCartType = {
+    item: CartItemType;
+    quantity: number;
 };
 
 const Cart = () => {
@@ -15,6 +20,31 @@ const Cart = () => {
     }
 
     const { cart } = cartCtx;
+
+    const [visibleCart, setVisibleCart] = useState<VisibleCartType[]>([]);
+
+    useEffect(() => {
+        let unique: string[] = [];
+        cart.forEach((cartItem) => {
+            if (!unique.includes(cartItem.name)) {
+                unique.push(cartItem.name);
+            }
+        });
+
+        let tempCart = [];
+
+        unique.forEach((dessert_name) => {
+            let counter = 0;
+            cart.forEach((cartItem) => {
+                if (cartItem.name === dessert_name) {
+                    counter++;
+                }
+            });
+            let visibleItem: VisibleCartType = {
+                item: 
+            }
+        });
+    }, [cartCtx]);
 
     return (
         <aside className="cart">
